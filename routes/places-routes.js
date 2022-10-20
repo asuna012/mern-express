aa15ed6a63060741bff784bccd7f7cb2a4d7fb00
Cholo <ctorralba012@gmail.com>
2022-10-20 15:09:12 +0800

    Finish all files except Google Geocoding API

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 const placesControllers = require("../controllers/places-controllers");
 
 router.get("/:pid", placesControllers.getPlaceByID);
 
-router.get("/user/:uid", placesControllers.getPlaceByUserID);
+router.get("/user/:uid", placesControllers.getPlacesByUserID);
 
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  placesControllers.createPlace
+);
+router.patch(
+  "/:pid",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  placesControllers.updatePlace
+);
+router.delete("/:pid", placesControllers.deletePlace);
 module.exports = router;
---
+++ routes/users-routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator");
+const userControllers = require("../controllers/users-controllers");
+
+router.get("/", userControllers.getUsers);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  userControllers.signup
+);
+router.post("/login", userControllers.login);
+module.exports = router;
