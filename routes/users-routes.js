8ee1ba2867cbf8e76ef93539aadc7c7f24502afb
Cholo <ctorralba012@gmail.com>
2022-11-08 09:38:35 +0800

    add multer for image upload

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const userControllers = require("../controllers/users-controllers");
-
+const fileUpload = require('../middleware/file-upload')
 router.get("/", userControllers.getUsers);
 router.post(
   "/signup",
+  fileUpload.single('image'),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
